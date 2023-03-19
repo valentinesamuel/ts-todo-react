7ef25279b69f36c50c990b8b3d778b69f5ea59ad
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-19 16:01:15 +0000

    FEAT: Created mui custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { FC, ReactElement } from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customTheme } from './themes/customeTheme';
 
 const App:FC = (): ReactElement => {
-  return <h1>Hwllo World</h1>;
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline/>
+      <h1>Hwllo World</h1>
+    </ThemeProvider>
+  );
 };
 export default App;
