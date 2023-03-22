0c7aa778d7587c42ed3fd3213d081b920a35073f
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-22 08:49:17 +0000

    FEAT: Added react query

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { FC, ReactElement } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { customTheme } from './themes/customeTheme';
 import { DashBoard } from './pages/dashboard';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <DashBoard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <DashBoard />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 export default App;
